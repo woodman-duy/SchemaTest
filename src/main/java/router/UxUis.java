2e938bd2a467611551715d8f089d5e5afc9a7283
Du Yong <danialdy@gmail.com>
2023-08-29 20:58:09 +0930

    Change schema

--- src/main/java/router/UxUis.java
+++ src/main/java/router/UxUis.java
@@ -5,9 +5,9 @@ import org.skyve.metadata.router.UxUi;
 // Only change theme attributes here.
 // Declare new UxUis in the DefaultUxUiSelector.
 public class UxUis {
-	public static final UxUi PHONE = UxUi.newPrimeFaces("phone", "editorial", "nova-light", null);
-	public static final UxUi TABLET = UxUi.newPrimeFaces("tablet", "editorial", "nova-light", null);
+	public static final UxUi PHONE = UxUi.newPrimeFaces("phone", "ecuador", "ecuador", "blue");
+	public static final UxUi TABLET = UxUi.newPrimeFaces("tablet", "ecuador", "ecuador", "blue");
 	public static final UxUi DESKTOP = UxUi.newSmartClient("desktop", "Tahoe", "casablanca");
-	public static final UxUi EXTERNAL = UxUi.newPrimeFaces("external", "editorial", "nova-light", null);
-	public static final UxUi STARTUP = UxUi.newPrimeFaces("startup", "editorial", "nova-light", null);
+	public static final UxUi EXTERNAL = UxUi.newPrimeFaces("external", "ecuador", "ecuador", "blue");
+	public static final UxUi STARTUP = UxUi.newPrimeFaces("startup", "ecuador", "ecuador", "blue");
 }
